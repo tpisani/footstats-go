b5c757449ab51e5b67dfb29759b1b17e603a7534
Thiago Pisani <pisani.thiago@gmail.com>
2016-02-21 12:57:55 -0300

    Added team data to matches, e.g. team IDs and scores

--- match.go
+++ match.go
@@ -16,25 +16,38 @@ const (
 // https://golang.org/src/time/format.go
 const footstatsDateLayout = "1/2/2006 3:04:05 PM"
 
-// TODO: Team data, e.g. team IDs and score.
 type Match struct {
-	FootstatsId     int64
-	Date            time.Time
-	Status          MatchStatus
-	Round           int
-	StadiumId       int64
-	RefereeId       int64
-	HasLiveCoverage bool
+	FootstatsId              int64
+	Date                     time.Time
+	Status                   MatchStatus
+	Round                    int
+	HomeTeamId               int64
+	HomeTeamScore            int
+	HomeTeamPenaltyScore     int
+	VisitingTeamId           int64
+	VisitingTeamScore        int
+	VisitingTeamPenaltyScore int
+	StadiumId                int64
+	RefereeId                int64
+	HasLiveCoverage          bool
 }
 
 type footstatsMatch struct {
-	FootstatsId     string `json:"@Id"`
-	Date            string `json:"Data"`
-	Status          string
-	Round           string `json:"Rodada"`
-	StadiumId       string `json:"IdEstadio"`
-	RefereeId       string `json:"IdArbitro"`
-	HasLiveCoverage string `json:"AoVivo"`
+	FootstatsId     string       `json:"@Id"`
+	Date            string       `json:"Data"`
+	Status          string       `json:"Status"`
+	Round           string       `json:"Rodada"`
+	Teams           []*matchTeam `json:"Equipe"`
+	StadiumId       string       `json:"IdEstadio"`
+	RefereeId       string       `json:"IdArbitro"`
+	HasLiveCoverage string       `json:"AoVivo"`
+}
+
+type matchTeam struct {
+	FootstatsId  string `json:"@Id"`
+	Score        string `json:"@Placar"`
+	PenaltyScore string `json:"@PlacarPenaltis"`
+	Type         string `json:"@Tipo"`
 }
 
 type matchData struct {
@@ -61,12 +74,12 @@ func (m *matchData) matches() []*Match {
 
 		var status MatchStatus
 		switch d.Status {
-		case "Partida em andamento":
-			status = OnGoing
+		case "Partida não iniciada":
+			status = NotStarted
 		case "Partida encerrada":
 			status = Finished
 		default:
-			status = NotStarted
+			status = OnGoing
 		}
 
 		var hasLiveCoverage bool
@@ -77,14 +90,37 @@ func (m *matchData) matches() []*Match {
 			hasLiveCoverage = false
 		}
 
+		var homeTeam, visitingTeam *matchTeam
+		for _, t := range d.Teams {
+			if t.Type == "Mandante" {
+				homeTeam = t
+			} else {
+				visitingTeam = t
+			}
+		}
+
+		homeTeamId, _ := strconv.ParseInt(homeTeam.FootstatsId, 10, 64)
+		homeTeamScore, _ := strconv.Atoi(homeTeam.Score)
+		homeTeamPenaltyScore, _ := strconv.Atoi(homeTeam.PenaltyScore)
+
+		visitingTeamId, _ := strconv.ParseInt(visitingTeam.FootstatsId, 10, 64)
+		visitingTeamScore, _ := strconv.Atoi(visitingTeam.Score)
+		visitingTeamPenaltyScore, _ := strconv.Atoi(visitingTeam.PenaltyScore)
+
 		matches = append(matches, &Match{
-			FootstatsId:     footstatsId,
-			Date:            date,
-			Status:          status,
-			Round:           round,
-			StadiumId:       stadiumId,
-			RefereeId:       refereeId,
-			HasLiveCoverage: hasLiveCoverage,
+			FootstatsId:              footstatsId,
+			Date:                     date,
+			Status:                   status,
+			Round:                    round,
+			HomeTeamId:               homeTeamId,
+			HomeTeamScore:            homeTeamScore,
+			HomeTeamPenaltyScore:     homeTeamPenaltyScore,
+			VisitingTeamId:           visitingTeamId,
+			VisitingTeamScore:        visitingTeamScore,
+			VisitingTeamPenaltyScore: visitingTeamPenaltyScore,
+			StadiumId:                stadiumId,
+			RefereeId:                refereeId,
+			HasLiveCoverage:          hasLiveCoverage,
 		})
 	}
 
