f59264638c31b64e1d94ff3713ca3a8084169271
Thiago Pisani <pisani.thiago@gmail.com>
2016-02-28 12:54:57 -0300

    Auth and championships retrieval tests

--- client_test.go
+++ client_test.go
@@ -5,21 +5,24 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"os"
+	"strings"
 
 	"testing"
 )
 
 var (
-	mux    *http.ServeMux
-	client *Client
-	server *httptest.Server
+	mux      *http.ServeMux
+	client   *Client
+	username string = "username"
+	password string = "password"
+	server   *httptest.Server
 )
 
 func setup() {
 	mux = http.NewServeMux()
 	server = httptest.NewServer(mux)
 
-	client = NewClient(server.URL, "username", "password")
+	client = NewClient(server.URL, username, password)
 }
 
 func teardown() {
@@ -32,6 +35,21 @@ func writeFileToResponse(w http.ResponseWriter, name string) {
 	w.Write(b)
 }
 
+func TestAuth(t *testing.T) {
+	setup()
+	defer teardown()
+
+	url := client.buildURL("test-endpoint", nil)
+
+	if !strings.Contains(url, "usuario="+username) {
+		t.Error("Missing username")
+	}
+
+	if !strings.Contains(url, "senha="+password) {
+		t.Error("Missing password")
+	}
+}
+
 func TestChampionships(t *testing.T) {
 	setup()
 	defer teardown()
@@ -40,8 +58,12 @@ func TestChampionships(t *testing.T) {
 		writeFileToResponse(w, "api-samples/championships.xml")
 	})
 
-	_, err := client.Championships()
+	championships, err := client.Championships()
 	if err != nil {
-		t.Fail()
+		t.Error("Unable to retrieve championships")
+	}
+
+	if clen := len(championships); clen != 2 {
+		t.Error("Expected 2 championships, got", clen)
 	}
 }
