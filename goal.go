7ae1cb119c975d9a57b8466bc41f411a529a9bb2
Thiago Pisani <pisani.thiago@gmail.com>
2016-02-29 08:59:14 -0300

    Initial structs of goal and stats

---
+++ goal.go
@@ -0,0 +1,55 @@
+package footstats
+
+import (
+	"strconv"
+)
+
+type Goal struct {
+	FootstatsId int64
+	PlayerId    int64
+	TeamId      int64
+	Period      MatchPeriod
+	Minute      int
+	Own         bool
+}
+
+type footstatsGoal struct {
+	FootstatsId string `json:"@Id"`
+	Period      string `json:"@Periodo"`
+	Minute      string `json:"@Momento"`
+	Type        string `json:"@Tipo"`
+	Player      *footstatsPlayer
+	Team        *footstatsTeam
+}
+
+func (f *footstatsGoal) goal() *Goal {
+	footstatsId, _ := strconv.ParseInt(f.FootstatsId, 10, 64)
+	minute, _ := strconv.Atoi(f.FootstatsId)
+	playerId, _ := strconv.ParseInt(f.Player.FootstatsId, 10, 64)
+	teamId, _ := strconv.ParseInt(f.Team.FootstatsId, 10, 64)
+
+	var period MatchPeriod
+	switch f.Period {
+	case "Primeiro tempo":
+		period = FirstHalf
+	case "Segundo tempo":
+		period = SecondHalf
+	}
+
+	var own bool
+	switch f.Type {
+	case "Contra":
+		own = true
+	default:
+		own = false
+	}
+
+	return &Goal{
+		FootstatsId: footstatsId,
+		PlayerId:    playerId,
+		TeamId:      teamId,
+		Period:      period,
+		Minute:      minute,
+		Own:         own,
+	}
+}
